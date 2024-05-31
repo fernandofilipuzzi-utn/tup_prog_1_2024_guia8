b5b9daf78829399716bd1b1627616a57a417bec6
fernandofilipuzzi.utn <fernandofilipuzzi.utn@gmail.com>
2024-05-31 08:52:13 -0300

    agregamos opción de ingreso de listado

--- Guia8/Ejercicio1/Program.cs
+++ Guia8/Ejercicio1/Program.cs
@@ -9,37 +9,80 @@ namespace Ejercicio1
     internal class Program
     {
         #region variables y métodos del dominio
-        int[] numeros=new int[100];
-        int contador = 0;
+        static int[] numeros=new int[100];
+        static int contador = 0;
 
+        static void CargarValor(int valor)
+        {
+            numeros[contador++] = valor;
+        }
         #endregion
 
         #region variables y métodos
-        static void MostrarMenu()
-        { 
+        static int MostrarMenuYSolicitarOpcion()
+        {
+            Console.Clear();
+
+            Console.WriteLine("1- Solicitar listado");
+            Console.WriteLine("2- Mostrar listado ordenado");
+            Console.WriteLine("3- Buscar valor");
+
+            int op =Convert.ToInt32(Console.ReadLine());
+            return op;
         }
 
-        static void MostrarCargaValore()
-        { }
+        static void MostrarSolicitudCargaValores()
+        {
+            Console.Clear();
+            Console.WriteLine("\t\t Solicitud de listado de valores");
+
+            Console.WriteLine("ingrese valor(-1 sale)");
+            int val = Convert.ToInt32(Console.ReadLine());
+            while (val>-1)
+            {
+                CargarValor(valor);
+
+                Console.WriteLine("ingrese valor(-1 sale)");
+                val = Convert.ToInt32(Console.ReadLine());
+            }
+
+            Console.WriteLine("presione una tecla para salir");
+            Console.ReadKey();
+        }
+
+        static void MostrarListadoOrdenado() 
+        {
+            Console.Clear();
+            Console.WriteLine("\t\t listado ordenado");
+
+            Console.WriteLine("presione una tecla para salir");
+            Console.ReadKey();
+        }
         #endregion
+
         static void Main(string[] args)
         {
             int op = 0;
 
-            MostrarMenu();
-            op = Convert.ToInt32(Console.ReadLine());
+            op= MostrarMenuYSolicitarOpcion();
             while (op == 0)
             {
                 switch (op)
                 {
                     case 1:
-                        { }break;
+                        {
+                            MostrarSolicitudCargaValores();
+                        }
+                        break;
+                    case 2:
+                        {
+                            MostrarListadoOrdenado();
+                        }break;
                     default:
                         { op = 0; }break;
                 }
 
-                MostrarMenu();
-                op = Convert.ToInt32(Console.ReadLine());
+                op = MostrarMenuYSolicitarOpcion();
             }
         }
     }
